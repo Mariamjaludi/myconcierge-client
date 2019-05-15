ba633909e2aef997efd4dbaeae74402d2c53ef06
mariamjaludi <mariam.jaludi@gmail.com>
2019-05-16 00:05:31 +0100

    findGuest function setup

--- src/App.js
+++ src/App.js
@@ -4,21 +4,34 @@ import './App.css';
 
 const HOTEL_API = 'http://localhost:3000/hotels'
 const GUEST_API = 'http://localhost:3000/guests'
+const LOGIN = 'http://localhost:3000/login'
 export default class App extends React.Component {
 
   state = {
     hotels: [],
-    guests: []
+    guest: null
   }
 
   componentDidMount(){
     fetch(HOTEL_API)
       .then(resp => resp.json())
       .then(hotels => this.setState({hotels}))
+  }
 
-      fetch(GUEST_API)
-        .then(resp => resp.json())
-        .then(guests => this.setState({guests}))
+  findGuest = (name, hotel_booking_id) => {
+    fetch(LOGIN ,
+      {
+          method: 'POST',
+          headers: {
+                      'Content-Type': 'application/json'
+                   },
+          body: JSON.stringify({
+                      name: name,
+                      hotel_booking_id: hotel_booking_id
+                })
+      })
+        .then( resp => resp.json() )
+        .then( student => this.setState( {guest} ) )
   }
 
   render () {
