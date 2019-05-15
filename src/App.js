000a7f05b956b158f82633885909a33a2a39b9e0
mariamjaludi <mariam.jaludi@gmail.com>
2019-05-15 23:46:17 +0100

    API fetch testing

--- src/App.js
+++ src/App.js
@@ -2,25 +2,43 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const HOTEL_API = 'http://localhost:3000/hotels'
+const GUEST_API = 'http://localhost:3000/guests'
+export default class App extends React.Component {
 
-export default App;
+  state = {
+    hotels: [],
+    guests: []
+  }
+
+  componentDidMount(){
+    fetch(HOTEL_API)
+      .then(resp => resp.json())
+      .then(hotels => this.setState({hotels}))
+
+      fetch(GUEST_API)
+        .then(resp => resp.json())
+        .then(guests => this.setState({guests}))
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
