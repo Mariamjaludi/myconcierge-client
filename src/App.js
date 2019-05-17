ec934ade6c5dd19d4199a789fe98170a083b362a
mariamjaludi <mariam.jaludi@gmail.com>
2019-05-18 00:28:45 +0100

    fixed fetch typo from students to guests

--- src/App.js
+++ src/App.js
@@ -31,26 +31,17 @@ export default class App extends React.Component {
                 })
       })
         .then( resp => resp.json() )
-        .then( student => this.setState( {guest} ) )
+        .then( guest => this.setState( {guest} ) )
+  }
+
+  renderFunction = () => {
+
   }
 
   render () {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.renderFunction()}
       </div>
     );
   }
